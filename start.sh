8f99b2451131ea16d90ce469ec54e39eeae234e8
Okko <142894633+eoovve@users.noreply.github.com>
2023-09-18 17:21:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-NEZHA_SERVER=${NEZHA_SERVER:-'aa.bbb.com'}           
+NEZHA_SERVER=${NEZHA_SERVER:-'nz.f4i.cn'}           
 NEZHA_PORT=${NEZHA_PORT:-'5555'}                      
-NEZHA_KEY=${NEZHA_KEY:-'zcJ45ZbryRRiS1Oopg'}          
+NEZHA_KEY=${NEZHA_KEY:-'Pr0xBHTAJvPgQvgZGD'}          
 NEZHA_TLS=${NEZHA_TLS:-''}                        
 ARGO_DOMAIN=${ARGO_DOMAIN:-''}         
 ARGO_AUTH=${ARGO_AUTH:-''}      
